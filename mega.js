83331c53fa4f988c186c1d5e2130f61e20961dd8
Nadeenpoorna-lkz <nadeepoorna987@gmail.com>
2025-02-09 17:54:50 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,8 +1,8 @@
 const mega = require("megajs");
 
 const auth = {
-    email: 'nadeenpoorna02@gmail.com',   //use your real vaild mega account email
-    password: 'Nadeen@1234',  ////use your real vaild mega account password
+    email: 'nadeenpoorna100@gmail.com',   //use your real vaild mega account email
+    password: 'Nadeen@123',  ////use your real vaild mega account password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
